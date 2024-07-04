be0b2d33a10509e24ae4ccdf08af98847061e530
wlynxg <liuauthor@foxmail.com>
2024-07-04 22:39:16 +0800

    feat: entering raw mode

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::io;
+use std::io::Read;
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+
 fn main() {
-    println!("Hello, world!");
+   enable_raw_mode().unwrap();
+    for byte in io::stdin().bytes() {
+        let b = byte.unwrap();
+        let c = b as char;
+
+        if c.is_control() {
+            println!("Binary: {0:08b} ASCII: {0:#03} \r", b);
+        } else {
+            println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r", b, c);
+        }
+
+        if c=='q' {
+            disable_raw_mode().unwrap();
+            break
+        }
+        println!("{}",c);
+    }
 }
